538ecf0c26d9d676ddc5b1d4b25dc194ba93455b
Zayn Patel <zaynpatelwhs@gmail.com>
2024-02-04 16:23:03 -0500

    Working udp stream for first 1024 bytes

--- GS_mini_avionics/GS_mini_avionics.ino
+++ GS_mini_avionics/GS_mini_avionics.ino
@@ -4,26 +4,26 @@
 
 /* INCLUDE LIBRARIES */
 #include <WiFi.h>
+#include <WiFiUDP.h>
 #include "SPIFFS.h"
 
+WiFiUDP udp;
+unsigned int localUdpPort = 4210;
+char incomingPacket[255];
 const char* ssid = "MiniAvionics";
-// const char* wpa = "123456555";
-const char* serverAddress = "http://192.168.4.1";
+const char* serverAddress = "192.168.4.1";
 
 WiFiServer server(80);
 
 void setup(){
   Serial.begin(115200);
 
-  // Set up the access point
-  WiFi.softAP(ssid);
-
-  // Start the server
-  server.begin();
+  WiFi.softAP(ssid);  // Set up the access point
+  udp.begin(localUdpPort);   // Start the server with a port to listen to
   
-  Serial.println("SoftAP started with IP: " + WiFi.softAPIP().toString());
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), localUdpPort);
 
-  // Check if the log file exists in the SPIFFS
+  // Check if the log file exists in the SPIFFS system on ESP32
   if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occured while mounting");
   } else {
@@ -33,31 +33,27 @@ void setup(){
   Serial.println(SPIFFS.exists("/4_21_2082_refactored.txt"));
 }
 
-void loop(){
-//   Check if a client has connected
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("New client connected");
-
-    // Open the file for reading
-    File file = SPIFFS.open("/4_21_2082_refactored.txt"); // file path for the SPIFF doesn't include same structure as computer file path
-
-    if (file) {
-      //Read and print the content of the file
-      while (file.available()) {
-        Serial.print(char(file.read()));
-      }
-
-      file.close();
-    } 
-
-    else {
-      Serial.println("Failed to open file");
+void loop() {
+  int packetSize = udp.parsePacket(); // parse the incoming packet size
+  if (packetSize) {
+    int len = udp.read(incomingPacket, 255);
+    if (len > 0) {
+      incomingPacket[len] = 0;
     }
+    Serial.printf("Received packet of size %d from %s:%d\n    (Hex: %s)\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort(), incomingPacket);
     
-    client.stop();
-    Serial.println("Client disconnected");
-  } else {
-    Serial.println("There is no client found.");
-  }
+    File file = SPIFFS.open("/4_21_2082_refactored.txt");
+    if (file) {
+      while (file.available()) {
+        char fileBuffer[128];
+        size_t bytesRead = file.readBytes(fileBuffer, sizeof(fileBuffer));
+        udp.beginPacket(udp.remoteIP(), udp.remotePort()); // prepares to start sending data over UDP
+        udp.write((const uint8_t*)fileBuffer, bytesRead);
+        udp.endPacket();
+      }
+      file.close();
+    } else {
+      Serial.println("Failed to open file for reading");
+    }
+}
 }
