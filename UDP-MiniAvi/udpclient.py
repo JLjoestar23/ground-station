d2ca8c62e3deaa533347ff8973a6bbd70b040a70
Zayn Patel <zaynpatelwhs@gmail.com>
2024-03-02 21:31:47 -0500

    Add python udp client/server scripts -- client works

--- UDP-MiniAvi/udpclient.py
+++ UDP-MiniAvi/udpclient.py
@@ -4,8 +4,7 @@ HOST = '192.168.4.1' # IP Address of the server
 PORT = 4210
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Testing client-server')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    while True:
+        message = input('Type something to send over socket.')
+        message = message.encode('utf-8')
+        s.sendto(message, (HOST, PORT))
