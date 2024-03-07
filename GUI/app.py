c1b10c940ab8010c784fe15dce64dfc2ae88e294
t0uk14 <covenanter32@gmail.com>
2024-03-06 19:09:32 -0500

    added updating graph functionality to GS website

--- GUI/app.py
+++ GUI/app.py
@@ -1,15 +1,15 @@
 import dash
-from dash import dcc, html
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
-import pandas as pd 
+import pandas as pd
+import numpy as np
+from dash.exceptions import PreventUpdate
+import plotly.graph_objs as go
+
 
 app = dash.Dash(__name__) # initialize the Dash app
 
-df = pd.DataFrame({
-    'x' : range(1, 11),
-    'y' : [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-})
-
+'''
 # function to customize plot characteristics
 def update_plot_layout(plot, xaxis_title, yaxis_title, title_text):
     plot.update_layout(
@@ -23,7 +23,9 @@ def update_plot_layout(plot, xaxis_title, yaxis_title, title_text):
             'font': {'size': 24}
         }
     )
+'''
 
+'''
 # Define plots
 alt_plot = px.line(df, x='x', y='y', title='Altitude Plot', height=450, width=600)
 velocity_plot = px.line(df, x='x', y='y', title='Velocity Plot', height=450, width=600)
@@ -39,22 +41,171 @@ update_plot_layout(temp_plot, "Time (s)", "Temperature (C)", "Temperature Plot")
 update_plot_layout(lin_accel_plot, "Time (s)", "Linear Acceleration (m/s^2)", "Linear Acceleration Plot")
 update_plot_layout(ang_accel_plot, "Time (s)", "Angular Acceleration (m/s^2)", "Angular Acceleration Plot")
 update_plot_layout(orientation_plot, "Time (s)", "Orientation", "Angular Acceleration Plot")
+'''
 
 # basically doing HTML bullshit in Python
 app.layout = html.Div([
+    dcc.Interval(
+        id = 'interval',
+        interval = 100,
+        n_intervals = 0,
+    ),
     html.H1('Main Telemetry Display', style={'textAlign': 'center', 'fontFamily': 'Arial'}),
     html.Div([
-        dcc.Graph(figure=alt_plot),
-        dcc.Graph(figure=velocity_plot),
-        dcc.Graph(figure=temp_plot)
+        dcc.Graph(
+            id='Altitude-Plot',
+            config={'displayModeBar': False}
+        ),
+        dcc.Graph(
+            id='Velocity-Plot',
+            config={'displayModeBar': False}
+        ),
+        dcc.Graph(
+            id='Temperature-Plot',
+            config={'displayModeBar': False}
+        )
     ], style={'display': 'flex', 'justifyContent': 'center', 'paddingBottom': '0px'}),
 
     html.Div([
-        dcc.Graph(figure=lin_accel_plot),
-        dcc.Graph(figure=ang_accel_plot),
-        dcc.Graph(figure=orientation_plot),
+        dcc.Graph(
+            id='LinearAccel-Plot',
+            config={'displayModeBar': False}
+        ),
+        dcc.Graph(
+            id='AngularAccel-Plot',
+            config={'displayModeBar': False}
+        ),
+        dcc.Graph(
+            id='Orientation-Plot',
+            config={'displayModeBar': False}
+        ),
     ], style={'display': 'flex', 'justifyContent': 'center'})
 ])
 
+
+# callback functions to update graphs
+@app.callback(
+    Output('Altitude-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_alt(n):
+    x = np.linspace(0, n/10, 1000)
+    y = (-0.5*(x-14)**2)+100 # placeholder value
+    if np.any(y < 0):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+    
+@app.callback(
+    Output('Velocity-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_vel(n):
+    x = np.linspace(0, n/10, 1000)
+    y = -x+14 # palceholder value
+    if np.any(y < -14):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='Velocity-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Velocity'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+    
+@app.callback(
+    Output('Temperature-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_temp(n):
+    x = np.linspace(0, n/10, 1000)
+    y = x # placeholder value
+    if np.any(y < 0):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='Temperature-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Temperature'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+    
+@app.callback(
+    Output('LinearAccel-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_lin_accel(n):
+    x = np.linspace(0, n/10, 1000)
+    y = x # placeholder value
+    if np.any(y < 0):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='LinearAccel-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Acceleration'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+
+@app.callback(
+    Output('AngularAccel-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_ang_accel(n):
+    x = np.linspace(0, n/10, 1000)
+    y = x # placeholder value
+    if np.any(y < 0):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='AngularAccel-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Acceleration'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+
+@app.callback(
+    Output('Orientation-Plot', 'figure'),
+    Input('interval', 'n_intervals')
+)
+def update_orientation(n):
+    x = np.linspace(0, n/10, 1000)
+    y = x # placeholder value
+    if np.any(y < 0):
+        raise PreventUpdate
+    else:
+        data = [go.Scatter(x=x, y=y, mode='lines')]
+        layout = go.Layout(
+            title='Orientation-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Orientation'),
+            height=500,
+            width=650
+        )
+        return {'data': data, 'layout': layout}
+
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
