274c30a96cf4c8444deb03af9147c7fc924086a3
t0uk14 <covenanter32@gmail.com>
2024-04-17 17:57:28 -0400

    added some UI elements

--- GUI/app.py
+++ GUI/app.py
@@ -15,6 +15,9 @@ plot_width = 550
 
 record_data = False
 recording_status = 'Begin Recording'
+connect_toggle = 'Connect'
+connect_status_text = 'Disconnected'
+connect_status = False
 
 # basically doing HTML bullshit in Python
 app.layout = html.Div(children=[
@@ -27,12 +30,14 @@ app.layout = html.Div(children=[
         
     # Div for header and top functions
     html.Div([
-        html.H1('Main Telemetry Display', style={'textAlign': 'center', 'fontFamily': 'Arial', "color": "#FFFFFF", 'margin': 'auto'}),
+        html.H1('Main Telemetry Display', style={'textAlign': 'center', 'fontFamily': 'Arial', "color": '#FFFFFF', 'margin': 'auto'}),
         ], style={'width': '25vw', 'height': '10vh', 'top': '0vh', 'left': '50vw', 'border': '5px solid white', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'margin': 'auto'}),
         
         html.Div([
             html.Button(recording_status, id='record_data_toggle'),
-        ], style={'width': '5vw', 'height': '2.5vw'}),
+            html.Button(connect_toggle, id='connect_toggle'),
+            html.H3(connect_status_text, style={'textAlign': 'center', 'fontFamily': 'Arial', "color": '#FFFFFF', 'margin': 'auto'}),
+        ], style={'width': '5vw', 'height': '2.5vw', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'margin': 'auto'}),
         
     # Div for 1st row of graphs
     html.Div([
@@ -92,6 +97,23 @@ def record_toggle(n_clicks, recording_status):
         print(record_data)
     return recording_status
 
+@app.callback(
+        Output('connect_toggle', 'children'),
+        Input('connect_toggle', 'n_clicks'),
+        State('connect_toggle', 'children')
+)
+def connect_toggle(n_clicks, connect_toggle):
+    global connect_status
+    if n_clicks:
+        if connect_toggle == 'Connect':
+            connect_toggle = 'Disconnect'
+            connect_status = True
+            #connect_status_text = 'Connected'
+        else:
+            connect_toggle = 'Connect'
+            connect_status = False
+            #connect_status_text = 'Disconnected'
+    return connect_toggle
 
 
 # callback functions to update graphs
@@ -103,11 +125,22 @@ def update_alt(n):
     x = np.linspace(0, n/10, 1000)
     y = (-0.5*(x-14)**2)+100 # placeholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
+    #else:
+        #data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
     else:
-        data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
-    else:
-        data = [go.Scatter(x=x, y=y, mode='lines')] # construct a scatter object (scatter plot with the information in layout)
+        data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))] # construct a scatter object (scatter plot with the information in layout)
         layout = go.Layout(
             title='Altitude-Plot', 
             xaxis=dict(title='Time'), 
@@ -128,7 +161,18 @@ def update_vel(n):
     x = np.linspace(0, n/10, 1000)
     y = -x+14 # palceholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
     else:
         data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
         layout = go.Layout(
@@ -151,7 +195,18 @@ def update_temp(n):
     x = np.linspace(0, n/10, 1000)
     y = x # placeholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
     else:
         data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
         layout = go.Layout(
@@ -174,7 +229,18 @@ def update_lin_accel(n):
     x = np.linspace(0, n/10, 1000)
     y = x # placeholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
     else:
         data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
         layout = go.Layout(
@@ -197,7 +263,18 @@ def update_ang_accel(n):
     x = np.linspace(0, n/10, 1000)
     y = x # placeholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
     else:
         data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
         layout = go.Layout(
@@ -220,7 +297,18 @@ def update_orientation(n):
     x = np.linspace(0, n/10, 1000)
     y = x # placeholder value
     if not record_data:
-        raise dash.exceptions.PreventUpdate
+        data = [go.Scatter(x=[], y=[], mode='lines', line=dict(color='#FF10F0'))]
+        layout = go.Layout(
+            title='Altitude-Plot', 
+            xaxis=dict(title='Time'), 
+            yaxis=dict(title='Altitude'),
+            height=plot_height,
+            width=plot_width,
+            plot_bgcolor='#303030',
+            paper_bgcolor='#303030',
+            font=dict(color='#FFFFFF')
+        )
+        return {'data': data, 'layout': layout}
     else:
         data = [go.Scatter(x=x, y=y, mode='lines', line=dict(color='#FF10F0'))]
         layout = go.Layout(
